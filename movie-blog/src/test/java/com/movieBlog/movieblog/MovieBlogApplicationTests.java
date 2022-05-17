3a763c8dc2a9fc59c0928bdb78a36bfe03b486fa
Jesus Arce <chuy_1322@hotmail.com>
2022-05-16 18:30:45 -0600

    Se agrego proyecto para comenzar con el desarrollo del servicio web con Java SpringBoot, se hicieron los archivos de propiedades para el ambiente de desarrollo y de produccion

---
+++ movie-blog/src/test/java/com/movieBlog/movieblog/MovieBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieBlog.movieblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
